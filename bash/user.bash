1240f5bded0ce26de1495d9e1c3f2e781cdf0e40
Bohdan Budnyy <bohdanxbudnyy@gmail.com>
2019-03-10 11:38:41 +0200

    * redesigned python virtual envs management (do not use activate, start   a subshell, virtual env can be changed, more sanity checks) * updated prompt (show virtual env) * updated pyenv config (do not modify environment repeatedly)

--- bash/user.bash
+++ bash/user.bash
@@ -2,7 +2,7 @@
 # prompt
 
 git_branch() {
-  if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]]; then
+  if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" ]; then
     branch=$(git branch | grep '^*' | colrm 1 2)
     if [ -z "$branch" ]; then  #empty repo
       branch="(empty)"
@@ -12,11 +12,17 @@ git_branch() {
 }
 
 git_dirty() {
-  if [[ $(git status --porcelain 2>/dev/null) ]]; then
+  if [ "$(git status --porcelain 2>/dev/null)" ]; then
     echo "*"
   fi
 }
 
+ps1_context() {
+  if [ -n "$VIRTUAL_ENV" ]; then
+    echo "($(basename "$VIRTUAL_ENV"))"
+  fi
+}
 
-PS1="\n\[\e[00;32m\]\u@\h \[\e[00;33m\]\w \[\e[00m\]\$(git_branch)\[\e[01;31m\]\$(git_dirty)\[\e[00m\]\n> "
+
+PS1="\n\[\e[00;32m\]\u@\h \[\e[00;33m\]\w \[\e[00m\]\$(git_branch)\[\e[01;31m\]\$(git_dirty)\[\e[00m\]\n$(ps1_context)> "
 
--- bash/venv.bash
+++ bash/venv.bash
@@ -1,39 +1,69 @@
 # python3 venv management
 
-export VENVDIR=$HOME/.python_venv
-if [ ! -d $VENVDIR ]; then
-  mkdir $VENVDIR
+export VENVDIR="$HOME/.python_venv"
+if [ ! -d "$VENVDIR" ]; then
+  mkdir "$VENVDIR"
 fi
 
+lsvenv() {
+  ls "$VENVDIR" -1
+}
+
 mkvenv() {
-  if [ -z "$1" ]
-    then
-      echo "usage: mkvenv <name>"
+  if [ -z "$1" ]; then
+    echo "usage: mkvenv <name>"
+  elif [ -d "$VENVDIR/$1" ]; then
+    echo "virtual environment ($1) already exists"
   else
-    python3 -m venv --copies $VENVDIR/"$1"
+    python3 -m venv --copies "$VENVDIR/$1"
+    echo "done"
   fi
 }
 
-lsvenv() {
-  ls $VENVDIR -1
-}
-
 avenv() {
-  if [ -z "$1" ]
-    then
-      echo "usage: avenv <name>"
+  if [ -z "$1" ]; then
+    echo "usage: avenv <name>"
   else
-    source $VENVDIR/"$1"/bin/activate
+    # if virtual env does not exist then create it first
+    if [ ! -d "$VENVDIR/$1" ]; then
+      echo "creating virtual environment ($1) ..."
+      python3 -m venv --copies "$VENVDIR/$1"
+    fi
+# instead of "source $VENVDIR/"$1"/bin/activate"
+# https://datagrok.org/python/activate/
+    # start a subshell for the first virtual env and replace the shell when
+    # virtual env is changed
+    if [ -z "$VIRTUAL_ENV" ]; then
+      (
+        # save oiginal path to correctly handle the case when virtual env is
+        # changed from within this virtual env
+        export _ORIGINAL_PATH="$PATH"
+        export VIRTUAL_ENV="$VENVDIR/$1"
+        export PATH="$VIRTUAL_ENV/bin:$_ORIGINAL_PATH"
+        unset PYTHONHOME
+        exec $SHELL
+      )
+    else
+        export VIRTUAL_ENV="$VENVDIR/$1"
+        export PATH="$VIRTUAL_ENV/bin:$_ORIGINAL_PATH"
+        unset PYTHONHOME
+        exec $SHELL
+    fi
   fi
 }
 
 rmvenv() {
-  if [ -z "$1" ]
-    then
-      echo "usage: rmvenv <name>"
+  if [ -z "$1" ]; then
+    echo "usage: rmvenv <name>"
+  elif [ -d "$VENVDIR/$1" ]; then
+    rm -rf "$VENVDIR/$1"
+    echo "done"
+    if [ "$(basename "$VIRTUAL_ENV")" = "$1" ] ; then
+      # if removing the active virtual env then quit it also
+      exit 0
+    fi
   else
-    deactivate 2>/dev/null
-    rm -rf $VENVDIR/"$1"
+    echo "virtual env ($1) not found, nothing to remove"
   fi
 }
 
