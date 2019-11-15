84315753142cd37d3ba933d8e45ed645fc1d7fc3
Bohdan Budnyy <ext-bohdan.budnyy@here.com>
2019-11-15 17:14:14 +0200

    changed fzf command to fd

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -2,5 +2,11 @@
 . /usr/share/fzf/completion.bash
 
 export FZF_DEFAULT_OPTS='--height 40% --multi --layout=reverse'
-export FZF_DEFAULT_COMMAND='rg --files --smart-case --hidden'
-export FZF_COMPLETION_TRIGGER="//"
+# find files under current directory
+export FZF_DEFAULT_COMMAND='fd --type f'
+# find files under current directory and append after cursor
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# find any directory and cd
+export FZF_ALT_C_COMMAND='fd --type d . /'
+export FZF_COMPLETION_TRIGGER='//'
+
