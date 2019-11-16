13169cdd516925f9871f77cf82e1b07c92075103
Bohdan Budnyy <bohdanxbudnyy@gmail.com>
2019-11-16 12:25:24 +0200

    updated fzf config

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -1,6 +1,3 @@
-. /usr/share/fzf/key-bindings.bash
-. /usr/share/fzf/completion.bash
-
 export FZF_DEFAULT_OPTS='--height 40% --multi --layout=reverse'
 # find files under current directory
 export FZF_DEFAULT_COMMAND='fd --type f'
