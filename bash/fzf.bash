2ded9ad9a526c2f3317502cb61927b3223101190
Bohdan Budnyy <ext-bohdan.budnyy@here.com>
2019-12-31 16:11:18 +0200

    include hidden file in fzf search

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -1,9 +1,9 @@
 export FZF_DEFAULT_OPTS='--height 40% --multi --layout=reverse'
 # find files under current directory
-export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_COMMAND='fd --hidden --type f'
 # find files under current directory and append after cursor
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # find any directory and cd
-export FZF_ALT_C_COMMAND='fd --type d . /'
+export FZF_ALT_C_COMMAND='fd --hidden --type d . /'
 export FZF_COMPLETION_TRIGGER='//'
 
