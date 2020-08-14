8b2ade631af0ee256c634be3b4e96b93cc74827d
Bohdan Budny <bohdanxbudnyy@gmail.com>
2020-08-14 09:47:49 +0300

    change bash prompt colors

--- bash/user.bash
+++ bash/user.bash
@@ -16,5 +16,5 @@ git_dirty() {
   fi
 }
 
-PS1="\n\[\e[00;32m\]\u@\h \[\e[00;33m\]\w \[\e[00m\]\$(git_branch)\[\e[01;31m\]\$(git_dirty)\[\e[00m\]\$(ps1_context)\n> "
+PS1="\n\[\e[00;34m\]\u@\h \[\e[00;32m\]\w \[\e[00m\]\$(git_branch)\[\e[01;31m\]\$(git_dirty)\[\e[00m\]\$(ps1_context)\n> "
 
