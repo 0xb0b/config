9fd47e296a4f6870094b2fe0f74f37968b0c6ddb
Bohdan Budny <bohdanxbudnyy@gmail.com>
2020-08-19 23:24:35 +0300

    * move ssh to separate bash module * refactor bash prompt

--- bash/user.bash
+++ bash/user.bash
@@ -1,10 +1,28 @@
-eval $(keychain --eval id_ed25519)
 
 . ~/config/bash/.bash_aliases
+. ~/config/bash/ssh.bash
 . ~/proj/envs/envs.bash
 . ~/config/bash/fzf.bash
 
 # prompt
+
+black='\e[0;30m'
+darkgray='\e[1;30m'
+red='\e[0;31m'
+lightred='\e[1;31m'
+green='\e[0;32m'
+lightgreen='\e[1;32m'
+orange='\e[0;33m'
+yellow='\e[1;33m'
+blue='\e[0;34m'
+lightblue='\e[1;34m'
+purple='\e[0;35m'
+lightpurple='\e[1;35m'
+cyan='\e[0;36m'
+lightcyan='\e[1;36m'
+lightgray='\e[0;37m'
+white='\e[1;37m'
+nocolor='\e[0m'
 
 git_branch() {
   if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" ]; then
@@ -12,15 +30,13 @@ git_branch() {
     if [ -z "$branch" ]; then  #empty repo
       branch="(empty)"
     fi
-    echo "$branch"
+    if [ "$(git status --porcelain 2>/dev/null)" ]; then
+      printf "${branch}${red}*"
+    else
+      printf "${branch}"
+    fi
   fi
 }
 
-git_dirty() {
-  if [ "$(git status --porcelain 2>/dev/null)" ]; then
-    echo "*"
-  fi
-}
-
-PS1="\n\[\e[00;34m\]\u@\h \[\e[00;32m\]\w \[\e[00m\]\$(git_branch)\[\e[01;31m\]\$(git_dirty)\[\e[00m\]\$(ps1_context)\n> "
+PS1="\n${blue}\u@\h ${nocolor}\w  ${cyan}\$(git_branch)${nocolor}\$(ps1_context)\n> "
 
